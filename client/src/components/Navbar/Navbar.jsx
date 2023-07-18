3a1d7a5a69b8903ca6cfe5173944a15bd344f0c7
keerthanyadava <keerthanyadav23@gmail.com>
2023-07-18 23:43:44 +0530

    Initial commit

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+function Navbarfunc() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand > OJ Keerthan</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to={'/'}>Problem SET</Nav.Link>
+            <Nav.Link as={Link} to={'/Login'}>Login</Nav.Link>
+            <Nav.Link as={Link} to={'/Signup'} >Signup</Nav.Link>
+            <Nav.Link as={Link} to={'/UserProfile'} >User Profile</Nav.Link>
+            <Nav.Link as={Link} to={'/Submissions'} >Submissions</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Navbarfunc;
---
+++ server/DB/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const connectDB = async () => {
+    try {
+      // Connection URL from MongoDB Atlas
+      const dbUrl = process.env.MONGO_DB_URL;
+  
+      // Connect to MongoDB Atlas
+      await mongoose.connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Connected to MongoDB Atlas');
+    } catch (error) {
+      console.error('Error connecting to MongoDB Atlas:', error);
+    }
+  };
+  
+  export default connectDB;
